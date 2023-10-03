e3ad712d0329747f29aa195b9e7b9c9622b3168b
eubro <eliasjunior1405@gmail.com>
2023-10-02 23:05:23 -0300

    finalização Blog do dev

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,35 @@
+export const dataFake=[
+    {
+
+        "id":"1",
+        "title": "A IA PROMETE MUDAR POR COMPLETO O MERCADO DE INFORMÁTICA",
+        "description":"Essa nova tecnologia veio para ajudar tanto os desenvolvedores, quanto os demais usúarios de diversas areas ",
+        "photoCover":"https://img.ibxk.com.br/2022/09/16/profissao-dev-16153228659058.jpg?ims=328x"
+
+    },
+    {
+
+        "id":"2",
+        "title": "SpaceX vem estudando a IA",
+        "description":"Elon Musk não iria ficar de fora dessa, sempre focado na evolução tecnologica...",
+        "photoCover":"https://akm-img-a-in.tosshub.com/businesstoday/images/story/202304/1.elon_musk_spacex.jpg_0-sixteen_nine_0.jpg?size=948:533"
+
+    },
+    {
+
+        "id":"3",
+        "title": "A META NA CORRIDA PARA OBTER MAIS CONHECIMENTO NA IA",
+        "description":"A meta vem buscando cada vez mais conhecimento sobre a tal tecnologia...",
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBsK0Qrf00nXF6M9znMVTBZlbTXo9CiP6H0Q&usqp=CAU"
+
+    },
+    {
+
+        "id":"4",
+        "title": "APPLE E A IA",
+        "description":"Que a Apple é uma grande marcar todo mundo sabe, mais e sua integração com a IA?...",
+        "photoCover":"https://olhardigital.com.br/wp-content/uploads/2019/11/20191120020457.jpg"
+
+    },
+  
+]
